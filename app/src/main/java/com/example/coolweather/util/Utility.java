17c50533b2ab7379522739a7f9f9f32a91c5d4ed
sxk <2698356823@qq.com>
2018-10-26 10:10:30 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -6,6 +6,8 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.Country;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -74,4 +76,17 @@ public class Utility {
         return false;
     }
 
+
+    //将返回的JSON数据解析成weather实体类
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherCount = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherCount,Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
